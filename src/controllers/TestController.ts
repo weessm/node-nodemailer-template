f65fa8f58799681e09b31c52edbe2f491bca9cf5
Weslei Miranda <wsmiranda11@gmail.com>
2023-05-13 17:18:05 -0300

    partials para email

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import { transporter, IMailOptions } from '../modules/sendMailModule';
 import { generate } from 'generate-password';
-import { hashSync, genSaltSync } from 'bcrypt';
 
 class TestController {
   async useMail(request: Request, response: Response) {
@@ -9,26 +8,24 @@ class TestController {
     const { to, subject, link, name } = request.body;
 
     // Gera senha provisória
-    const salt = genSaltSync(10);
     const generatedPassword = generate({
       length: 10,
       numbers: true,
       symbols: true,
     });
-    const senhaProvisoria = hashSync(generatedPassword, salt);
 
     // Define as opções do email com interface IMailOptions
     const mailOptions: IMailOptions = {
       to: to,
-      from: `MailTest <${process.env.SMTP_USER}>`,
+      from: `Equipe X <${process.env.SMTP_USER}>`,
       subject: subject,
-      text: `Olá, ${name}, essa é sua senha provisória: ${senhaProvisoria}, entre e altere em: ${link}`,
+      text: `Olá, ${name}, essa é sua senha provisória: ${generatedPassword}, entre e altere em: ${link}`,
       template: 'primeiroacesso',
       context: {
         link,
         subject,
         name,
-        senha: senhaProvisoria,
+        senha: generatedPassword,
       },
     };
 
